9f322b4e508ed8f092a03e980b01103c447e2456
nada <nadamoafia66@gmail.com>
2025-10-29 14:27:27 +0300

    news APi implementation

---
+++ lib/home/category/source_tab_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:news/home/category/widget/source_name.dart';
+import 'package:news/model/SourceResponse.dart';
+import 'package:news/utils/App_colors.dart';
+import '../news/news_widget.dart';
+class SourceTabWidget extends StatefulWidget {
+  final List<Source> sourcesList;
+
+  const SourceTabWidget({super.key, required this.sourcesList});
+
+  @override
+  State<SourceTabWidget> createState() => _SourceTabWidgetState();
+}
+
+class _SourceTabWidgetState extends State<SourceTabWidget> {
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: widget.sourcesList.length,
+      child: Column(
+        children: [
+          TabBar(
+            tabAlignment: TabAlignment.start,
+            dividerColor: AppColors.transparent,
+            onTap: (index) {
+              selectedIndex = index;
+              setState(() {});
+            },
+            indicatorColor: Theme.of(context).indicatorColor,
+            isScrollable: true,
+            tabs: widget.sourcesList.map((source) {
+              return SourceName(
+                source: source,
+                selected: selectedIndex == widget.sourcesList.indexOf(source),
+              );
+            }).toList(),
+          ),
+
+          Expanded(
+            child: NewsWidget(source: widget.sourcesList[selectedIndex]),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:news/home/category/category_details.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text("Home"))),
+      body: CategoryDetails(),
+    );
   }
 }
