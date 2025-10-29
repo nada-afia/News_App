bf8b4611528b22696eb9ed3122f6efb6497ddfb2
nada <nadamoafia66@gmail.com>
2025-10-29 13:59:31 +0300

    Add splash screen feature

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.homeScreen,
       routes: {
         AppRoutes.homeScreen: (context) => HomeScreen(),
